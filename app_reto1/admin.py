f85f20fdfbc576a2e7f10d1666b846e3e689b622
Hezitzaile <pablo.moraza@opendeusto.es>
2023-04-19 11:16:50 +0200

    super usuario creado y vista de prueba

--- app_reto1/admin.py
+++ app_reto1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Empleado , Proveedor , Planta, Equipo, Urgencia, Tipo_ticket, Estado_ticket, Ticket
 
-# Register your models here
+admin.site.register(Empleado)
+admin.site.register(Proveedor)
+admin.site.register(Planta)
+admin.site.register(Equipo)
+admin.site.register(Urgencia)
+admin.site.register(Tipo_ticket)
+admin.site.register(Estado_ticket)
+admin.site.register(Ticket)
