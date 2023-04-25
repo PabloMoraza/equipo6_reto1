2f7dab24e76e6b3fb0bd5e71e55694c07e19d099
Hezitzaile <pablo.moraza@opendeusto.es>
2023-04-25 14:38:16 +0200

    Views y urls de la app

--- app_reto1/urls.py
+++ app_reto1/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.prueba, name='prueba'),
+    # path('', views.prueba, name='prueba'),
+        path('', views.EquipoListView.as_view(), name='equipos'),
+        path('equipos/<int:pk>/', views.EquipoDetailView.as_view(), name='equipo'),
+        path('', views.TicketListView.as_view(), name='tickets'),
+        path('ticket/<int:pk>/', views.TicketDetailView.as_view(), name='ticket'),
 ]
--- app_reto1/views.py
+++ app_reto1/views.py
@@ -1,3 +1,27 @@
-from django.http import HttpResponse
-def prueba(request):
-    return HttpResponse("PRUEBA!")
+from django.views.generic import ListView, DetailView
+from equipo6_reto1.app_reto1.models import Equipo, Ticket
+
+
+
+# PRUEBA PARA VER QUE SE VISUALIZA EN LA VENTANA
+# ------------------------------------------------
+# from django.http import HttpResponse
+# def prueba(request):
+#     return HttpResponse("PRUEBA!")
+
+# ----------------------------------------------------
+# VISTAS DE EQUIPO
+class EquipoListView(ListView):
+    model = Equipo
+
+class EquipoDetailView(DetailView):
+   model = Equipo
+# ------------------------------------------------------
+# # ----------------------------------------------------
+# VISTAS DE TICKET
+class TicketListView(ListView):
+    model = Ticket
+
+class TicketDetailView(DetailView):
+   model = Ticket
+# ------------------------------------------------------
