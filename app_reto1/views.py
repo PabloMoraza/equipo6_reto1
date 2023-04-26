98bb5d24c523b7fc8b9afe94188a588d01ed47db
Hezitzaile <pablo.moraza@opendeusto.es>
2023-04-26 11:04:28 +0200

    se ve la pantalla de prueba

--- app_reto1/views.py
+++ app_reto1/views.py
@@ -5,23 +5,23 @@ from .models import Equipo, Ticket
 
 # PRUEBA PARA VER QUE SE VISUALIZA EN LA VENTANA
 # ------------------------------------------------
-# from django.http import HttpResponse
-# def prueba(request):
-#     return HttpResponse("PRUEBA!")
+from django.http import HttpResponse
+def prueba(request):
+    return HttpResponse("PRUEBA!")
 
-# ----------------------------------------------------
-# VISTAS DE EQUIPO
-class EquipoListView(ListView):
-    model = Equipo
-
-class EquipoDetailView(DetailView):
-   model = Equipo
-# ------------------------------------------------------
 # # ----------------------------------------------------
-# VISTAS DE TICKET
-class TicketListView(ListView):
-    model = Ticket
+# # VISTAS DE EQUIPO
+# class EquipoListView(ListView):
+#     model = Equipo
 
-class TicketDetailView(DetailView):
-   model = Ticket
-# ------------------------------------------------------
+# class EquipoDetailView(DetailView):
+#    model = Equipo
+# # ------------------------------------------------------
+# # # ----------------------------------------------------
+# # VISTAS DE TICKET
+# class TicketListView(ListView):
+#     model = Ticket
+
+# class TicketDetailView(DetailView):
+#    model = Ticket
+# # ------------------------------------------------------
