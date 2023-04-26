30608fc5700602b125ad689259fc8e4290e22bd3
Martin Pavon Campos <martin.pavon@opendeusto.es>
2023-04-26 16:52:19 +0200

    pruebas martin

--- app_reto1/urls.py
+++ app_reto1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     # path('', views.prueba, name='prueba'),
+#     path('', views.EquipoListView.as_view(), name='equipos'),
+# ]
+
 urlpatterns = [
-    # path('', views.prueba, name='prueba'),
-    path('', views.EquipoListView.as_view(), name='equipos'),
+     path('', views.index_empleados, name ='empleados'),
+     path('empleados/<int:empleado_id>/', views.show_empleados, name = "detalle empleado" ),
+     path('plantas/<int:planta_id>/equipos',views.index_equipos_por_planta, name = "equiposxplanta")
 ]
--- app_reto1/views.py
+++ app_reto1/views.py
@@ -1,27 +1,44 @@
 from django.views.generic import ListView, DetailView
-from .models import Equipo, Ticket
-
-
-
-# PRUEBA PARA VER QUE SE VISUALIZA EN LA VENTANA
-# ------------------------------------------------
+from .models import Empleado, Planta, Equipo
 from django.http import HttpResponse
-def prueba(request):
-    return HttpResponse("PRUEBA!")
+# from .models import Equipo, Ticket
 
-# ----------------------------------------------------
-# VISTAS DE EQUIPO
-class EquipoListView(ListView):
-    model = Equipo
+# # PRUEBA PARA VER QUE SE VISUALIZA EN LA VENTANA
+# # ------------------------------------------------
+# from django.http import HttpResponse
+# def prueba(request):
+#     return HttpResponse("PRUEBA!")
 
-class EquipoDetailView(DetailView):
-   model = Equipo
-# ------------------------------------------------------
 # # ----------------------------------------------------
-# VISTAS DE TICKET
-class TicketListView(ListView):
-    model = Ticket
+# # VISTAS DE EQUIPO
+# class EquipoListView(ListView):
+#     model = Equipo
 
-class TicketDetailView(DetailView):
-   model = Ticket
-# ------------------------------------------------------
+# class EquipoDetailView(DetailView):
+#    model = Equipo
+# # ------------------------------------------------------
+# # # ----------------------------------------------------
+# # VISTAS DE TICKET
+# class TicketListView(ListView):
+#     model = Ticket
+
+# class TicketDetailView(DetailView):
+#    model = Ticket
+# # ------------------------------------------------------
+
+# Prueba martin listado empleados
+def index_empleados(request):
+    empleados = Empleado.objects.order_by("apellido1")
+    output = ", ".join([d.apellido1 for d in empleados])
+    return HttpResponse(output)
+# Prueba martin datos de un empleado
+def show_empleados(request, empleado_id):
+    empleado = Empleado.objects.get(pk=empleado_id)
+    output = f'{empleado.apellido1} {empleado.apellido2}, {empleado.nombre} DNI: {empleado.DNI} Contacto: {empleado.email} {empleado.telefono}'
+    return HttpResponse(output)
+# Prueba martin equipos por planta
+def index_equipos_por_planta(request, planta_id):
+    planta = Planta.objects.get(pk=planta_id)
+    output = ", ".join([f"{e.num_serie} {e.marca} {e.modelo}" for e in planta.equipo_set.all()])
+    # output = ", ".join(["{e.num_serie} {e.marca} {e.modelo}" for e in planta.equipo_set.all()])
+    return HttpResponse(output)
