067f192f0a08218d4f1a1ad8b58a13ee6d420a1c
Martin Pavon Campos <martin.pavon@opendeusto.es>
2023-05-24 19:32:39 +0200

    paginad o de 2 elementos

--- app_reto1/views.py
+++ app_reto1/views.py
@@ -5,26 +5,38 @@ from .models import Ticket, Urgencia, Tipo_ticket, Estado_ticket, Empleado, Equi
 from django.views import View
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 from app_reto1.forms import TicketForm, EquipoForm, EmpleadoForm, ProveedorForm
-
+from django.core.paginator import Paginator
+from django.http import Http404
 
 
 # Lista de tickets
 def index_ticket(request):
     busqueda = request.GET.get("buscar")
     tickets = get_list_or_404(Ticket.objects.order_by("fecha_apertura"))
+    page = request.GET.get("page", 1)
+
+    try:
+        paginator = Paginator(tickets, 2)
+        tickets = paginator.page(page)
+    except:
+        raise Http404
+
     if busqueda:
         tickets = Ticket.objects.filter(
-            Q(num_referencia__icontains = busqueda) |
-            Q(fecha_apertura__icontains = busqueda) |
-            Q(urgencia__gravedad__icontains = busqueda) |
-            Q(estado_ticket__estado__icontains = busqueda) |
-            Q(equipo_a_reparar__modelo__icontains = busqueda) |
-            Q(empleado_asignado__DNI__icontains = busqueda) |
-            Q(tipo_ticket__tipo__icontains = busqueda)
+            Q(num_referencia__icontains=busqueda) |
+            Q(fecha_apertura__icontains=busqueda) |
+            Q(urgencia__gravedad__icontains=busqueda) |
+            Q(estado_ticket__estado__icontains=busqueda) |
+            Q(equipo_a_reparar__modelo__icontains=busqueda) |
+            Q(empleado_asignado__DNI__icontains=busqueda) |
+            Q(tipo_ticket__tipo__icontains=busqueda)
         ).distinct()
-    
-    context = {"lista_tickets": tickets}
-    return render(request, "ticket_list.html", context)
+    data = {
+        "lista_tickets": tickets,
+        "paginator": paginator
+    }
+
+    return render(request, "ticket_list.html", data)
 
 # datos de un ticket
 
@@ -78,24 +90,31 @@ class TicketDeleteView(DeleteView):
 
 # Lista de Equipos
 
-
 def index_equpos(request):
-     busqueda = request.GET.get("buscar")
-     equipos = get_list_or_404(Equipo.objects.order_by("num_serie"))
-     if busqueda:
-      equipos= Equipo.objects.filter(
-            Q(num_serie__icontains= busqueda) |
-            Q(marca__icontains = busqueda) |
-            Q(modelo__icontains = busqueda) |
-            Q(tipo_equipo__icontains = busqueda) |
-            Q(fecha_adquisicion__icontains = busqueda) |
-            Q(fecha_puesta_marcha__icontains = busqueda) |
-            Q(proveedor__nombre__icontains = busqueda) |
-            Q(planta__nombre__icontains = busqueda)
+    busqueda = request.GET.get("buscar")
+    equipos = get_list_or_404(Equipo.objects.order_by("num_serie"))
+    page = request.GET.get("page", 1)
+    try:
+        paginator = Paginator(equipos, 2)
+        equipos = paginator.page(page)
+    except:
+        raise Http404
 
-     ).distinct()
-     context = {"lista_equipos": equipos}
-     return render(request, "equipo_list.html", context)
+    if busqueda:
+        equipos = Equipo.objects.filter(
+            Q(num_serie__icontains=busqueda) |
+            Q(marca__icontains=busqueda) |
+            Q(modelo__icontains=busqueda) |
+            Q(tipo_equipo__icontains=busqueda) |
+            Q(fecha_adquisicion__icontains=busqueda) |
+            Q(fecha_puesta_marcha__icontains=busqueda) |
+            Q(proveedor__nombre__icontains=busqueda) |
+            Q(planta__nombre__icontains=busqueda)
+
+        ).distinct()
+    data = {"lista_equipos": equipos,
+            "paginator" : paginator}
+    return render(request, "equipo_list.html", data)
 
 # datos de un Equipos
 
@@ -147,22 +166,30 @@ class EquipoDeleteView(DeleteView):
     success_url = "/aplicacion/equipos/"
 
 
-
 # Lista de empleados
 def index_empleados(request):
     busqueda = request.GET.get("buscar")
     empleados = get_list_or_404(Empleado.objects.order_by("DNI"))
+    page = request.GET.get("page", 1)
+    try:
+        paginator = Paginator(empleados, 2)
+        empleados = paginator.page(page)
+    except:
+        raise Http404
+
     if busqueda:
         empleados = Empleado.objects.filter(
-            Q(DNI__icontains = busqueda) |
-            Q(nombre__icontains = busqueda) |
-            Q(apellido1__icontains = busqueda) |
-            Q(apellido2__icontains = busqueda) |
-            Q(email__icontains = busqueda) |
-            Q(telefono__icontains = busqueda) 
+            Q(DNI__icontains=busqueda) |
+            Q(nombre__icontains=busqueda) |
+            Q(apellido1__icontains=busqueda) |
+            Q(apellido2__icontains=busqueda) |
+            Q(email__icontains=busqueda) |
+            Q(telefono__icontains=busqueda)
         ).distinct()
-    context = {"lista_empleados": empleados}
-    return render(request, "empleado_list.html", context)
+
+    data = {"lista_empleados": empleados,
+            "paginator": paginator}
+    return render(request, "empleado_list.html", data)
 
 
 # Datos de empleado
@@ -217,13 +244,22 @@ class EmpleadoDeleteView(DeleteView):
 def index_proveedores(request):
     busqueda = request.GET.get("buscar")
     proveedores = get_list_or_404(Proveedor.objects.order_by("nombre"))
+    page = request.GET.get("page", 1)
+    try:
+        paginator = Paginator(proveedores, 2)
+        proveedores = paginator.page(page)
+    except:
+        raise Http404
+    
+
     if busqueda:
         proveedores = Proveedor.objects.filter(
-            Q(nombre__icontains = busqueda) |
-            Q(telefono__icontains = busqueda) 
+            Q(nombre__icontains=busqueda) |
+            Q(telefono__icontains=busqueda)
         ).distinct()
-    context = {"lista_proveedores": proveedores}
-    return render(request, "proveedores_list.html", context)
+    data = {"lista_proveedores": proveedores,
+            "paginator" : paginator}
+    return render(request, "proveedores_list.html", data)
 
 # Datos de Proveedores
 
