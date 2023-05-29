c5665b3665b17c341877759eb4825bae9999d1aa
Martin Pavon Campos <martin.pavon@opendeusto.es>
2023-05-29 22:07:07 +0200

    API creado

--- app_reto1/static/js/api_proveedores.js
+++ app_reto1/static/js/api_proveedores.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/aplicacion/proveedores/"
+const API_URL = "http://127.0.0.1:8000/aplicacion/proveedores_api/"
 
 fetch(API_URL)
     .then(response => response.json())
